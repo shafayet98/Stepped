00d19c959cb6ccf4a7bd06786eca0d8ccaf749d4
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2019-02-24 19:35:13 +0600

    login view, signup view, bmi calc
    login view, signup view, bmi calc  added

--- app/src/main/java/com/example/stepped_01/MainActivity.java
+++ app/src/main/java/com/example/stepped_01/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.stepped_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView signupId;
+    private Button loginId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
+        signupId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+        loginId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BmiCalculatorActivity.class));
+            }
+        });
+
+    }
+
+    public void init(){
+        signupId = findViewById(R.id.signupId);
+        loginId = findViewById(R.id.loginId);
     }
 }
