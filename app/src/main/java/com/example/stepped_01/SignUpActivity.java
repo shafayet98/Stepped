947006fd74e549ac34d648f93b91d760649ea8b0
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2019-02-25 22:05:54 +0600

    services activity added

--- app/src/main/java/com/example/stepped_01/SignUpActivity.java
+++ app/src/main/java/com/example/stepped_01/SignUpActivity.java
@@ -1,13 +1,35 @@
 package com.example.stepped_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private Button signUpId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        init();
+
+        signUpId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, ServicesActivity.class));
+            }
+        });
+
+
+    }
+
+    public void init(){
+
+        signUpId = findViewById(R.id.signUpId);
+
+
     }
 }
