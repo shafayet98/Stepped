b20cfe4c39674c8243c2126e346553787adc8d47
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-02-25 20:17:24 +0600

    result bmi activity initiated

--- app/src/main/java/com/example/stepped_01/BmiCalculatorActivity.java
+++ app/src/main/java/com/example/stepped_01/BmiCalculatorActivity.java
@@ -1,13 +1,36 @@
 package com.example.stepped_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BmiCalculatorActivity extends AppCompatActivity {
+
+
+    private Button calculateBmiId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
+
+        init();
+
+        calculateBmiId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BmiCalculatorActivity.this, ResultBmiActivity.class));
+            }
+        });
+
     }
+
+    public void init(){
+
+        calculateBmiId = findViewById(R.id.calculateBmiId);
+
+    }
+
 }
---
+++ app/src/main/java/com/example/stepped_01/ResultBmiActivity.java
@@ -0,0 +1,13 @@
+package com.example.stepped_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultBmiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_bmi);
+    }
+}
