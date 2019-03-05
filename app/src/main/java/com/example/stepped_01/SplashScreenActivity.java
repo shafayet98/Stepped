b477d247bf9f731c3accedf624aaf06a9b671454
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-03-05 22:39:05 +0600

    splash screen added

---
+++ app/src/main/java/com/example/stepped_01/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.stepped_01;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView image = findViewById(R.id.image);
+        image.startAnimation(AnimationUtils.loadAnimation(SplashScreenActivity.this, R.anim.splash_screen_animation));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            }
+        }, 3000);
+    }
+}
