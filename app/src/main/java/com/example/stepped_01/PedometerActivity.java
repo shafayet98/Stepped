045bdfed5c637c5c8547de47b71a675032205ac9
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-03-08 10:50:23 +0600

    notification service initiated

--- app/src/main/java/com/example/stepped_01/PedometerActivity.java
+++ app/src/main/java/com/example/stepped_01/PedometerActivity.java
@@ -2,12 +2,38 @@ package com.example.stepped_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PedometerActivity extends AppCompatActivity {
+
+    private ProgressBar pedometerProgressBar;
+    private TextView stepsTextView;
+    private TextView kilometerTextView;
+    private TextView minutesTextView;
+    private TextView totalCalorieTextView;
+    private Button setGoalsButton;
+    private TextView goalsTextView;
+    private Button weeklyProgressButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedometer);
+        init();
+
+
+    }
+
+    private void init(){
+        pedometerProgressBar = findViewById(R.id.pedometerProgressBar);
+        stepsTextView = findViewById(R.id.stepsTextView);
+        kilometerTextView = findViewById(R.id.kilometerTextView);
+        minutesTextView = findViewById(R.id.kilometerTextView);
+        totalCalorieTextView = findViewById(R.id.totalCalorieTextView);
+        setGoalsButton = findViewById(R.id.setGoalsButton);
+        goalsTextView = findViewById(R.id.goalsTextView);
+        weeklyProgressButton = findViewById(R.id.weeklyProgressButton);
     }
 }
