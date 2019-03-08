737fb84e9e03e60781c109825741a1e5a70cc5a5
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-03-08 11:53:34 +0600

    alert dialog added to pedometer
    custom alert dialog added to the setGoals button

--- app/src/main/java/com/example/stepped_01/PedometerActivity.java
+++ app/src/main/java/com/example/stepped_01/PedometerActivity.java
@@ -1,8 +1,12 @@
 package com.example.stepped_01;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -23,8 +27,44 @@ public class PedometerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pedometer);
         init();
 
+        setGoalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertUserForStepGoals();
+            }
+        });
 
     }
+
+    private void alertUserForStepGoals(){
+        final View view = getLayoutInflater().inflate(R.layout.custom_alert_dialog, null);
+        final EditText stepsAlertTextView = view.findViewById(R.id.stepsAlertTextView);
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(PedometerActivity.this);
+        builder1.setTitle("Set your step goals: ");
+        builder1.setMessage("The more you give the better");
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        goalsTextView.setText(stepsAlertTextView.getText().toString());
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.setView(view);
+        alert11.show();
+    }
+
+
 
     private void init(){
         pedometerProgressBar = findViewById(R.id.pedometerProgressBar);
