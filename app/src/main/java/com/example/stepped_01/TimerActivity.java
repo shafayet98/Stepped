b6f400f985368a0f4a4de6534ee53c0590f7171d
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2019-03-18 20:58:52 +0600

    Timer Added
    1) Gui update of timer
    2) Timer start functionality added
    3) Timer Pause functionality added

--- app/src/main/java/com/example/stepped_01/TimerActivity.java
+++ app/src/main/java/com/example/stepped_01/TimerActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class TimerActivity extends AppCompatActivity {
 
     private Button timerStartId;
+    private EditText hourId, minId, secId;
 
 
     @Override
@@ -20,12 +22,23 @@ public class TimerActivity extends AppCompatActivity {
         timerStartId.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(TimerActivity.this,TimerStartActivity.class));
+                Intent intent = new Intent(TimerActivity.this, TimerStartActivity.class);
+                intent.putExtra("hour", Integer.parseInt(hourId.getText().toString()));
+                intent.putExtra("min", Integer.parseInt(minId.getText().toString()));
+                intent.putExtra("sec", Integer.parseInt(secId.getText().toString()));
+                startActivity(intent);
             }
         });
     }
 
     public void init(){
         timerStartId = findViewById(R.id.timerStartId);
+        hourId = findViewById(R.id.hourId);
+        minId = findViewById(R.id.minId);
+        secId = findViewById(R.id.secId);
+    }
+
+    private void validation(){
+
     }
 }
--- app/src/main/java/com/example/stepped_01/TimerStartActivity.java
+++ app/src/main/java/com/example/stepped_01/TimerStartActivity.java
@@ -1,13 +1,113 @@
 package com.example.stepped_01;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class TimerStartActivity extends AppCompatActivity {
+
+    int hour, min, sec;
+    int finalHour, finalMin, finalSec;
+    private TextView showTimerText;
+    private CountDownTimer countDownTimer;
+    private boolean isPaused = false;
+    int remaining = 0;
+    int totalMil = 0;
+    private Button pauseId;
+    private static final String FORMAT = "%02d:%02d:%02d";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_start);
+
+        showTimerText = findViewById(R.id.showTimerText);
+        pauseId = findViewById(R.id.pauseId);
+
+        int hour = getIntent().getIntExtra("hour", 0);
+        int min = getIntent().getIntExtra("min", 0);
+        int sec = getIntent().getIntExtra("sec", 0);
+
+
+        finalHour = hour*3600000;
+        finalMin = min*60000;
+        finalSec = sec*1000;
+
+        totalMil = finalHour+finalMin+finalSec;
+
+        countDownTimer = new CountDownTimer(totalMil, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+
+                showTimerText.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+
+                remaining += 1000;
+            }
+
+            public void onFinish() {
+                showTimerText.setText("done!");
+            }
+        }.start();
+
+
+
+//        Toast.makeText(getApplicationContext(), hour + " " + min + " "+ sec + "", Toast.LENGTH_SHORT).show();
+
+        pauseId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(countDownTimer != null && pauseId.getText().toString().equals("Pause")){
+                    countDownTimer.cancel();
+                    isPaused = true;
+                    totalMil = totalMil - remaining;
+                    remaining = 0;
+                    pauseId.setText("Start");
+                    countDownTimer = null;
+                    return;
+                }
+
+                if(pauseId.getText().toString().equals("Start")){
+                    if(isPaused && countDownTimer == null){
+                        int r = totalMil - remaining;
+                        remaining = 0;
+                        pauseId.setText("Pause");
+                        countDownTimer = new CountDownTimer(r, 1000) { // adjust the milli seconds here
+
+                            public void onTick(long millisUntilFinished) {
+
+                                showTimerText.setText(""+String.format(FORMAT,
+                                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+
+                                remaining += 1000;
+                            }
+
+                            public void onFinish() {
+                                showTimerText.setText("done!");
+                            }
+                        }.start();
+                    }
+                }
+            }
+        });
+
+
+
+
     }
 }
