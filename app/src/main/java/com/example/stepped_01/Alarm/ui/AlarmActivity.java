591b56c0d023f33e45a7b34881ceeb47f8f9150a
MD. RISHADUL ISLAM KHAN <rishadulislam@outlook.com>
2019-03-31 20:54:13 +0600

    file name is refactored

--- app/src/main/java/com/example/stepped_01/Alarm/ui/AlarmActivity.java
+++ app/src/main/java/com/example/stepped_01/Alarm/ui/AlarmActivity.java
@@ -9,7 +9,7 @@ public class AlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alaram);
+        setContentView(R.layout.activity_alarm);
     }
 
 }
